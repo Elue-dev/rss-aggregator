9eb21a8000af77784acf4603a4bf3209eeb2c657
Wisdom <eluewisdom@gmail.com>
2023-11-11 22:40:18 +0100

    feat: auth middleware, get user by api key logic

---
+++ handler_user.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/elue-dev/rss-aggregator/internal/auth"
+	"github.com/elue-dev/rss-aggregator/internal/database"
+	"github.com/google/uuid"
+)
+
+type parameters struct {
+	Name string `json:"name"`
+}
+
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing json %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt:  time.Now().UTC(),
+		Name: params.Name,
+	})
+
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Could not create user %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseUserToUserModel(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUserByAPIKey(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetApiKey(r.Header)
+
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Could not get user %v", err))
+		return
+	}
+
+	respondWithJSON(w, 200, databaseUserToUserModel(user))
+}
--- main.go
+++ main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/elue-dev/rss-aggregator/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
 type errResponse struct {
 	Error string `json:"error"`
 }
 
+type apiConfig struct {
+	DB *database.Queries
+}
+
 func respondWithError(w http.ResponseWriter, code int, msg string) {
 	if code > 499 {
-		log.Println("Responding with 5 error:", msg)
+		log.Println("Responding with 5XX error:", msg)
 	}
 
 	respondWithJSON(w, code, errResponse{
@@ -30,9 +38,22 @@ func main() {
 	godotenv.Load(".env")
 
 	port := os.Getenv("PORT")
-
 	if port == "" {
 		log.Fatal("port could not be found in env")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL could not be found in env")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Failed to connect to database", err)
+	}
+
+	apiCfg := apiConfig{
+		DB:  database.New(conn),
 	}
 
 	router := chi.NewRouter()
@@ -50,6 +71,8 @@ func main() {
 
 	  v1Router.Get("/healthz", handlerReadiness)
 	  v1Router.Get("/err", hanndlerErr)
+	  v1Router.Post("/users", apiCfg.handlerCreateUser)
+	  v1Router.Get("/users", apiCfg.handlerGetUserByAPIKey)
 
 	  router.Mount("/v1", v1Router)
 
@@ -59,7 +82,7 @@ func main() {
 	}
 
 	log.Printf("Server starting on port %v", port)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
